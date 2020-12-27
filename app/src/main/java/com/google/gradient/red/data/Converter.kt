6dc2ac04953df7d449c9cf7154d3f64ca0170185
gradientdev <xinyunzhouu@gmail.com>
2020-12-27 14:55:54 -0500

    Removed placeholder image in row layout

--- app/src/main/java/com/google/gradient/red/data/Converter.kt
+++ app/src/main/java/com/google/gradient/red/data/Converter.kt
@@ -19,14 +19,14 @@ class Converter {
     }
 
     @TypeConverter
-    fun fromBitmap(bitmap: Bitmap): ByteArray {
+    fun fromBitmap(bitmap: Bitmap?): ByteArray? {
         val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        bitmap?.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
         return outputStream.toByteArray()
     }
 
     @TypeConverter
-    fun toBitmap(byteArray: ByteArray): Bitmap {
-        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+    fun toBitmap(byteArray: ByteArray?): Bitmap? {
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray?.size!!)
     }
 }
